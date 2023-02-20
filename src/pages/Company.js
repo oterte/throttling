f76dc59f5a457544e81082b5da0404bf05a28651
oterte <97093479+oterte@users.noreply.github.com>
2023-02-20 10:54:48 +0900

    throttling & debouncing -1

---
+++ src/pages/Company.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Company() {
+  return (
+    <div>컴패니</div>
+  )
+}
+
+export default Company
---
+++ src/pages/Home.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+function Home() {
+
+    let timerId = null;
+    const [state, setState] = useState();
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+        return () => {
+            // 언마운트 시 특정 동작 가능
+            // home 컴포넌트가 사라질때 특정 동작 가능
+            if(timerId){
+                clearTimeout(timerId)
+            }
+        }
+    }, [])
+    
+    const throttle = (delay) => {
+        if(timerId){
+            // timerId가 있으면 바로 함수 종료
+            return;
+        }
+        // 스로틀링이 발동할 때 리렌더링 -> timerId가 무조건 null이 됨-> 이 스로틀링은 정상작동하지 않음
+        setState(!state);
+
+
+
+        console.log(`API 요청 실행 ${delay}ms 동안 추가 요청은 안받는다`)
+        timerId = setTimeout(() => {
+            console.log(`${delay}ms 지남. 추가요청 받음`)
+            timerId = null;
+        }, delay)
+    }
+
+    // 반복적인 이벤트 이후 이벤트 딜레이가 지나면 실행되는 함수
+    const debounce = (delay) => {
+        if(timerId){
+            // 할당되어 있는 timerId에 해당하는 타이머 제거
+             clearTimeout(timerId)
+        }
+        timerId = setTimeout(() => {
+            console.log(`마지막 요청으로부터 ${delay}ms 지났으므로 API 요청 실행`)
+            timerId = null;
+        },delay)
+    }
+  return (
+    <div style={{
+        paddingLeft:20,
+        paddingRight:20
+    }}>
+
+
+
+        <h1>Button 이벤트 예제</h1>
+        <button onClick={() => throttle(2000)}>스로틀링 버튼</button>
+        <button onClick={() => debounce(2000)}>디바운싱 버튼</button>
+        <button onClick={(() => {navigate('company')})}>페이지 이동</button>
+    </div>
+  )
+}
+
+export default Home
